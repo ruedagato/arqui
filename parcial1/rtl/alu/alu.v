5210575648ecb5debe00fbb1144d848535809b46
ruedagato <ruedagato@gmail.com>
2015-08-21 12:29:18 -0500

    alu

--- parcial1/rtl/alu/alu.v
+++ parcial1/rtl/alu/alu.v
@@ -1,53 +1,44 @@
-module  mux
+module  alu
 	//Parametros
 	#(
 		parameter N=16
 	)
 	//entradas y salidas
 	(
-		input wire [1:0] selecM,
-		input wire [N-1:0] R_0,
-		input wire [N-1:0] R_1,
-		input wire [N-1:0] R_2,
-		input wire [N-1:0] R_3,
-		output wire [N-1:0] Q,
+		input wire [N-1:0] i_a,
+		input wire [N-1:0] i_b,
+		input wire [1:0] i_control,
+		output wire [N-1:0] q
 	);
-	 parameter [1:0] P_N=2'b00;
-	 parameter [1:0] P_0=2'b10;
-	 parameter [1:0] P_1=2'b01;
-	 parameter [1:0] P_2=2'b11;
+	 parameter [1:0] suma=2'b00;
+	 parameter [1:0] resta=2'b10;
+	 parameter [1:0] shift_d=2'b01;
+	 parameter [1:0] shift_i=2'b11;
 
 	//registros y señales
 	reg [N-1:0] salida;
 
 	//cuerpo del modulo
-	always @(selecM ) begin
-		case(selecM) begin
-			P_N: begin
-			
-			end
-			P_0:begin
-			
-			end
-			P_1:begin
-				
-			end
-			P_2:begin
-				
-			end
-			default:begin
-				
-			end
-
-			
-		endcase
-			
+	always @(*) begin
+			case(i_control)
+				suma: begin
+					salida = i_a +i_b;
+				end
+				resta: begin
+					salida = i_a - i_b;
+				end
+				shift_i: begin
+					salida= i_a<<1;
+				end
+				shift_d:begin
+					salida=i_a>>1;
+				end
+			endcase
 		end
 	end
 
 	//asiganaciones
-	assign o_signal = ((i_a>=i_b))?1'b1:1'b0;
-	assign o_resta = i_a-i_b;
+	assign q = salida;
 
 
 endmodule 
