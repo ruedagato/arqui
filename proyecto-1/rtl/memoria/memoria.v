7e69473fbab80cf7f59e26625cb4a49c74b81631
ruedagato <ruedagato@gmail.com>
2015-09-09 22:15:15 -0500

    serie terminada

---
+++ proyecto-1/rtl/memoria/memoria.v
@@ -0,0 +1,111 @@
+module memoria
+	//Parametros
+	#(
+		parameter N=16
+	)
+	//entradas y salidas
+	(
+		// entradas
+		input wire w,rst,clk,
+		input wire [3:0] select_register,
+		input wire [N-1:0] s,
+		// salidas
+		output wire [N-1:0]r1,
+		output wire [N-1:0]r2,
+		output wire [N-1:0]r3,
+		output wire [N-1:0]r4,
+		output wire [N-1:0]r5,
+		output wire [N-1:0]r6,
+		output wire [N-1:0]r7,
+		output wire [N-1:0]r8,
+		output wire [N-1:0]r9,
+		output wire [N-1:0]r10,
+		output wire [N-1:0]r11,
+		output wire [N-1:0]r12,
+		output wire [N-1:0]r13,
+		output wire [N-1:0]r14,
+		output wire [N-1:0]r15,
+		output wire [N-1:0]r16
+	);
+
+	//registros y señales
+	reg [15:0]reg1; 
+	reg [15:0]reg2;
+	reg [15:0]reg3;
+	reg [15:0]reg4;
+	reg [15:0]reg5;
+	reg [15:0]reg6;
+	reg [15:0]reg7;
+	reg [15:0]reg8;
+	reg [15:0]reg9;
+	reg [15:0]reg10;
+	reg [15:0]reg11;
+	reg [15:0]reg12;
+	reg [15:0]reg13;
+	reg [15:0]reg14;
+	reg [15:0]reg15;
+	reg [15:0]reg16;
+
+	//cuerpo del modulo
+	always @(posedge rst, posedge clk) 
+	begin
+		if (rst) begin
+			// reset
+			reg1 = 16'b0000000000000001;
+			reg2 = 16'b0000000000000001;
+			reg3 = 16'b0000000000000001;
+			reg4 = 16'b0000000000000001;
+			reg5 = 16'b0000000000000000;
+			reg6 = 16'b0000000000000000;
+			reg7 = 16'b0000000000000000;
+			reg8 = 16'b0000000000000000;
+			reg9 = 16'b0000000000000000;
+			reg10 = 16'b0000010000000100;
+			reg11 = 16'b0000000000000100;
+			reg12 = 16'b0000000000000100;
+			reg13 = 16'b0000000000000100;
+			reg14 = 16'b1000000000000100;
+			reg15 = 16'b1010001000000100;
+			reg16 = 16'b1000000000000100;
+		end
+		else if (w) begin
+			case(select_register)
+				4'b0000:reg1 = s;
+				4'b0001:reg2 = s;
+				4'b0010:reg3 = s;
+				4'b0011:reg4 = s;
+				4'b0100:reg5 = s;
+				4'b0101:reg6 = s;
+				4'b0110:reg7 = s;
+				4'b0111:reg8 = s;
+				4'b1000:reg9 = s;
+				4'b1001:reg10 = s;
+				4'b1010:reg11 = s;
+				4'b1011:reg12 = s;
+				4'b1100:reg13 = s;
+				4'b1101:reg14 = s;
+				4'b1110:reg15 = s;
+				4'b1111:reg16 = s;
+			endcase
+		end
+	end
+
+	// asignaciónes
+	assign r1 =		reg1;
+	assign r2 =		reg2;
+	assign r3 =		reg3;
+	assign r4 =		reg4;
+	assign r5 =		reg5;
+	assign r6 =		reg6;
+	assign r7 =		reg7;
+	assign r8 =		reg8;
+	assign r9 =		reg9;
+	assign r10 =	reg10;
+	assign r11 =	reg11;
+	assign r12 =	reg12;
+	assign r13 =	reg13;
+	assign r14 =	reg14;
+	assign r15 =	reg15;
+	assign r16 =	reg16;
+
+endmodule
---
+++ proyecto-1/system.v
@@ -0,0 +1,179 @@
+//---------------------------------------------------------------------------
+// SharkBoad SystemModule
+//
+// Top Level Design for the Xilinx Spartan 3-100E Device
+//---------------------------------------------------------------------------
+
+/*#
+# SharkBoad
+# Copyright (C) 2012 Bogotá, Colombia
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#*/
+
+module system
+#(
+	parameter	clk_freq	= 50000000,
+	parameter	uart_baud_rate	= 57600,
+	parameter	p_N = 16
+) (
+	input		clk,
+	input		rst,
+
+	// UART
+	//input             uart_rxd, 
+	//output            uart_txd,
+	// Debug 
+	output		led,
+	output[p_N-1:0] salidas_final
+);
+//---------------------------------------------------------------------------
+// General Purpose IO
+//---------------------------------------------------------------------------
+wire counter_unit0_ovf;
+wire led_out;
+
+
+	// 	14	13		12  11  10  9		8  7  6  5 		4  3  2  1 		0
+    //
+    //	[0	0		0	0   0   0		0  0  0  0		0  0  0  0		0]
+    // 	cnt_alu		slc_mux_a			slc_mux_b		slc_reg			w
+
+// cables del sitema del divisor
+wire mayor;
+wire [14:0] signal_control;
+wire [p_N-1:0]in_register,rr1,rr2,rr3,rr4,rr5,rr6,rr7,rr8,rr9,rr10,rr11,rr12,rr13,rr14,rr15,rr16,i_a,i_b;
+
+
+
+
+memoria
+	//Parametros
+	#(
+		.N(16)
+	)
+	registro
+	//entradas y salidas
+	(
+		// entradas
+		.w(signal_control[0]),
+		.rst(rst),
+		.clk(clk),
+		.select_register(signal_control[4:1]),
+		.s(in_register),
+		// salidas
+		.r1(rr1),
+		.r2(rr2),
+		.r3(rr3),
+		.r4(rr4),
+		.r5(rr5),
+		.r6(rr6),
+		.r7(rr7),
+		.r8(rr8),
+		.r9(rr9),
+		.r10(rr10),
+		.r11(rr11),
+		.r12(rr12),
+		.r13(rr13),
+		.r14(rr14),
+		.r15(rr15),
+		.r16(rr16)
+	);
+mux
+	//Parametros
+	#(
+		.N(p_N)
+	)
+	muxa
+	//entradas y salidas
+	(
+		.selecm(signal_control[12:9]),
+		.R_0(rr1),
+		.R_1(rr2),
+		.R_2(rr3),
+		.R_3(rr4),
+		.R_4(rr5),
+		.R_5(rr6),
+		.R_6(rr7),
+		.R_7(rr8),
+		.R_8(rr9),
+		.R_9(rr10),
+		.R_10(rr11),
+		.R_11(rr12),
+		.R_12(rr13),
+		.R_13(rr14),
+		.R_14(rr15),
+		.R_15(rr16),
+		.q(i_a)
+	);
+
+mux
+	//Parametros
+	#(
+		.N(p_N)
+	)
+	muxb
+	//entradas y salidas
+	(
+		.selecm(signal_control[8:5]),
+		.R_0(rr1),
+		.R_1(rr2),
+		.R_2(rr3),
+		.R_3(rr4),
+		.R_4(rr5),
+		.R_5(rr6),
+		.R_6(rr7),
+		.R_7(rr8),
+		.R_8(rr9),
+		.R_9(rr10),
+		.R_10(rr11),
+		.R_11(rr12),
+		.R_12(rr13),
+		.R_13(rr14),
+		.R_14(rr15),
+		.R_15(rr16),
+		.q(i_b)
+	);
+
+alu
+	//Parametros
+	#(
+		.N(p_N)
+	)
+	alu
+	//entradas y salidas
+	(
+		.i_a(i_a),
+		.i_b(i_b),
+		.i_control(signal_control[14:13]),
+		.mayor(mayor),
+		.q(in_register)
+	);
+
+control
+	cont
+   (
+   	//input
+    .clk(clk), 
+    .rst(rst),
+    .mayor(mayor),
+    //output
+    .o_signal(signal_control)
+   );
+//----------------------------------------------------------------------------
+// Wires Assigments
+//----------------------------------------------------------------------------
+assign led = led_out;
+assign salidas_final = rr3;
+
+endmodule
